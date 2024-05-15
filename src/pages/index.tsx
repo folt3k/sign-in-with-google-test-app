b58970a1ec3cb814ab8d550ff3fadcf320ca2b45
folt3k <folt3k@gmail.com>
2024-05-15 07:41:21 +0200

    done frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
+import Script from "next/script";
+import { useEffect, useRef } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        "679975214581-ibtp5leaap1qgcmdvg5flb8aop4g73jh.apps.googleusercontent.com",
+      callback: handleCredentialResponse,
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("googleSignInBtnWrapper")!,
+      { type: "standard", shape: "pill" },
+    );
+  }, []);
+
+  const handleCredentialResponse = (
+    response: google.accounts.id.CredentialResponse,
+  ) => {
+    console.log(response);
+  };
+
   return (
     <>
-      <h1>Home</h1>
+      <div id="googleSignInBtnWrapper" style={{maxWidth: 240}}></div>
+      <Script
+        src="https://accounts.google.com/gsi/client"
+        strategy="beforeInteractive"
+      />
     </>
   );
 }
