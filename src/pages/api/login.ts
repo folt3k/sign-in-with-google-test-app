0f0ff876d7792d1e864b0806f2e2ff3bf3cafa2d
folt3k <folt3k@gmail.com>
2024-05-30 16:49:16 +0200

    comments

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,9 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 import { OAuth2Client, TokenPayload } from "google-auth-library";
 import jwt from "jsonwebtoken";
 
+// inicjujemy klienta Prismy i OAuth2
 const prisma = new PrismaClient();
 const authClient = new OAuth2Client();
 
@@ -15,6 +15,7 @@ export default async function handler(
     const { idToken } = req.body;
 
     try {
+      // weryfikujemy czy token ID jest na pewno poprawny
       const payload = await verifyIdToken(idToken);
       const userId = payload.sub;
       const userEmail = payload.email;
@@ -25,6 +26,7 @@ export default async function handler(
         },
       });
 
+      // jeżeli użytkownik nie istnieje, tworzymy go
       if (!user) {
         user = await prisma.user.create({
           data: {
@@ -34,6 +36,7 @@ export default async function handler(
         });
       }
 
+      // generujemy token JWT
       const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!);
 
       res.status(200).json({ token });
@@ -44,6 +47,8 @@ export default async function handler(
 }
 
 async function verifyIdToken(idToken: string): Promise<TokenPayload> {
+  // ta walidacja jest rekomendowana przez Google dla zwiększenia bezpieczeństwa,
+  // ponieważ ID token mógł zostać zmieniony po stronie klienta
   const ticket = await authClient.verifyIdToken({
     idToken: idToken,
     audience: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import Script from "next/script";
 
 export default function Home() {
+  // inicjujemy klienta Sign In With Google
   const initGoogleSignIn = () => {
     google.accounts.id.initialize({
+      // dodajemy wygnenerowany wcześniej Client ID z Google API
       client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      // callback wywoływany po zalogowaniu się przez użytkownika
       callback: handleCredentialResponse,
     });
+    // renderujemy przycisk logowania
     google.accounts.id.renderButton(
       document.getElementById("googleSignInBtnWrapper")!,
       { type: "standard", shape: "pill" },
@@ -27,6 +31,7 @@ export default function Home() {
     console.log(data);
   };
 
+  // ładujemy skrypt i po jego wczytaniu inicjujemy klienta Sign In With Google
   return (
     <>
       <div id="googleSignInBtnWrapper" />
